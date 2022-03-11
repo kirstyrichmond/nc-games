dc083c29e00fa1828c357fdfb46c534377eed1e3
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-03-11 11:09:36 +0000

    merged 2 components into 1

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,26 @@
 import { Box, CircularProgress } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../components/Contexts/User-Context";
 import "../styles/home.css";
 import { getUsers } from "../utils/api";
-import UserLogin from "./UserLogin";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
-
   const [users, setUsers] = useState([]);
+  const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUsers().then((usersFromApi) => {
+      setUsers(usersFromApi);
+    });
+  }, []);
+
+  const login = (newUser) => {
+    setLoggedInUser(newUser);
+    navigate(`/categories`);
+  };
 
   useEffect(() => {
     getUsers().then((userList) => {
@@ -17,13 +30,52 @@ const Home = () => {
   }, []);
 
   return (
-  // isLoading ? (
-  //   <Box sx={{ display: "flex", justifyContent: "center" }}>
-  //     <CircularProgress size={65} className="loading-spinner" />
-  //   </Box>
-  // ) : (
+    // isLoading ? (
+    //   <Box sx={{ display: "flex", justifyContent: "center" }}>
+    //     <CircularProgress size={65} className="loading-spinner" />
+    //   </Box>
+    // ) : (
     <div>
-      <UserLogin />
+      <ul className="list-all-users">
+        {users.map((user) => {
+          return (
+            <li className="list-item" key={user.username}>
+              <div className="user-list-user">
+                <div>
+                  <img
+                    className="user-list-photo"
+                    src={user.avatar_url}
+                    alt={user.username}
+                  />
+                </div>
+                <div className="user-list-name-photo">
+                  <div className="user-list-name">
+                    <h3>{user.name}</h3>
+                  </div>
+                  <div>
+                    {loggedInUser === null ||
+                    loggedInUser.username !== user.username ? (
+                      <button
+                        className="user-button"
+                        onClick={() => login(user)}
+                      >
+                        Log in
+                      </button>
+                    ) : (
+                      <button
+                        className="user-button"
+                        onClick={() => login(user)}
+                      >
+                        Continue...
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
